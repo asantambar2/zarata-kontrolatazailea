d16f6b71bb3bcaa495dc26293b08e7fe77241c24
asantambar2 <131778320+asantambar2@users.noreply.github.com>
2023-05-02 14:49:12 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+let soinulimitea = 125
+basic.forever(function () {
+    if (input.soundLevel() > soinulimitea) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
+    }
+})
